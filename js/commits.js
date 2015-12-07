54f362e084ad52f0297b2ccd235d812dc75cdb27
Alyson <emerald.aly@gmail.com>
2015-12-07 00:16:56 -0500

    add files

---
+++ js/commits.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $(document).on("click", "a", function(e) {
+    e.preventDefault();
+    alert("you clicked a link"),
+    $.ajax({
+      type: "GET",
+      url: $(this).attr("href")
+      success: function(commits){
+        $("tbody").empty();
+        for(var i=0; i <commits.length; i++) {
+            $("tbody").append(buildTableRow(commits[i]));
+
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorThrown);
+        alert("Something went wrong"),
+      }
+      
+  });
+    function buildTableRow(commitData) {
+      var fullNameTd =$("<td>").append(commitData.fullNameTd);
+      var commitMsgTd =$("<td>").append(commitData.commit);
+      var dateTd =$("<td>").append(commitData.dateTd);
+      var authorTd =$("<td>").append(commitData.author.login);
+
+        $("<tr").append(commitMsgTd)
+        .append(dateTd)
+        .append(authorTd)
+    }
+});
+
+});
