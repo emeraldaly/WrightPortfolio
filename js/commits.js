ee0ecafdf49eb300b0d8a34e7f1df59e64225bbd
dannyvassallo <danielvassallo87@gmail.com>
2015-12-07 19:48:21 -0500

    fixed typos in commits / repos js and compressed images

--- js/commits.js
+++ js/commits.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
   $(document).on("click", "a", function(e) {
+    var thisHref = $(this).attr("href");
     e.preventDefault();
     alert("you clicked a link"),
     $.ajax({
       type: "GET",
-      url: $(this).attr("href")
+      url: thisHref,
       success: function(commits){
         $("tbody").empty();
         for(var i=0; i <commits.length; i++) {
             $("tbody").append(buildTableRow(commits[i]));
 
+        }
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log(jqXHR);
         console.log(textStatus);
         console.log(errorThrown);
-        alert("Something went wrong"),
+        alert("Something went wrong");
       }
-      
+
   });
     function buildTableRow(commitData) {
       var fullNameTd =$("<td>").append(commitData.fullNameTd);
